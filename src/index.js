4a998f9d72802874a1e729d8c2d00e7afc456eaf
Brent Bowen <bodunk2k@aol.com>
2020-03-16 18:32:26 -0500

    reset back think I need to close and reopen

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'node_modules/bootstrap/dist/css/Bootstrap.min.css';
+import 'bootstrap/dist/css/Bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
