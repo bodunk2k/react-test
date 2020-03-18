76df573038f6b74a66250fe269acdf97dabb53e3
Brent Bowen <bodunk2k@aol.com>
2020-03-17 20:51:32 -0500

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-
+import Directory from './components/DirectoryComponent.js';
 import './App.css';
 
 class App extends Component {
@@ -11,7 +11,9 @@ class App extends Component {
               <div className="container">
                   <NavbarBrand href="/">NuCamp</NavbarBrand>
               </div>
+              <Directory />
               </Navbar>
+              
           </div>
       );
   }
